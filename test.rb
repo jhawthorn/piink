0a5e79a14a5e4842e0f00322eaf208370a73a95b
John Hawthorn <john@hawthorn.email>
2020-03-03 01:05:38 -0800

    WIP

--- test.rb
+++ test.rb
@@ -1,2 +1,135 @@
 require "rpi_gpio"
 
+EPD_WIDTH       = 800
+EPD_HEIGHT      = 480
+
+
+RESET_PIN = RST_PIN  = 17
+DC_PIN   = 25
+CS_PIN   = 8
+BUSY_PIN = 24
+
+
+require "spi"
+
+
+RPi::GPIO.set_numbering :bcm
+
+RPi::GPIO.setup RST_PIN,  as: :output
+RPi::GPIO.setup DC_PIN,   as: :output
+RPi::GPIO.setup CS_PIN,   as: :output
+RPi::GPIO.setup BUSY_PIN, as: :input
+
+SPIDEV = SPI.new(device: '/dev/spidev0.0')
+SPIDEV.speed=4000000
+
+def reset
+  RPi::GPIO.set_high RESET_PIN
+  sleep 0.2
+  RPi::GPIO.set_low RESET_PIN
+  sleep 0.004
+  RPi::GPIO.set_high RESET_PIN
+  sleep 0.2
+end
+
+
+def send_command(command)
+  RPi::GPIO.set_low DC_PIN
+  RPi::GPIO.set_low CS_PIN
+  SPIDEV.xfer(txdata: [command])
+  RPi::GPIO.set_high CS_PIN
+end
+
+def send_data(data)
+  RPi::GPIO.set_high DC_PIN
+  RPi::GPIO.set_low CS_PIN
+  SPIDEV.xfer(txdata: [data])
+  RPi::GPIO.set_high CS_PIN
+end
+
+def read_busy
+  puts "e-Paper busy"
+  send_command(0x71)
+  while RPi::GPIO.high?(BUSY_PIN)
+    send_command(0x71)
+  end
+  sleep 0.2
+  puts "done"
+end
+
+def init
+  reset
+
+  send_command(0x01)			#POWER SETTING
+  send_data(0x07)
+  send_data(0x07)    #VGH=20V,VGL=-20V
+  send_data(0x3f)		#VDH=15V
+  send_data(0x3f)		#VDL=-15V
+
+  send_command(0x04) #POWER ON
+
+  sleep 0.1
+
+  read_busy
+
+  send_command(0x00)			#PANNEL SETTING
+  send_data(0x0F)   #KW-3f   KWR-2F	BWROTP 0f	BWOTP 1f
+
+  send_command(0x61)        	#tres
+  send_data(0x03)		#source 800
+  send_data(0x20)
+  send_data(0x01)		#gate 480
+  send_data(0xE0)
+
+  send_command(0x15)
+  send_data(0x00)
+
+  send_command(0x50)			#VCOM AND DATA INTERVAL SETTING
+  send_data(0x11)
+  send_data(0x07)
+
+  send_command(0x60)			#TCON SETTING
+  send_data(0x22)
+end
+
+def clear
+  total_bytes = EPD_WIDTH * EPD_HEIGHT / 8
+
+  send_command(0x10)
+  total_bytes.times do
+    send_data(0xff)
+  end
+
+  send_command(0x13)
+  total_bytes.times do
+    send_data(0x00)
+  end
+
+  send_command(0x12)
+  sleep 0.1
+  read_busy
+end
+
+def display(imageblack, imagered)
+  self.send_command(0x10)
+  total_bytes.times do
+    send_data(0xff)
+  end
+  for i in range(0, int(self.width * self.height / 8)):
+  self.send_data(imageblack[i]);
+        
+        self.send_command(0x13)
+        for i in range(0, int(self.width * self.height / 8)):
+            self.send_data(~imagered[i]);
+        
+        self.send_command(0x12)
+        epdconfig.delay_ms(100)
+        self.ReadBusy()
+end
+
+init
+
+clear
+
+
+RPi::GPIO.reset
